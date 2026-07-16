a62f8d21e21c47fd266fa71a9597c59263cf76ae
amyheather <a.heather2@exeter.ac.uk>
2026-07-16 15:43:08 +0100

    style(ruff): ran ruff format and ruff check --fix

--- ambforecast/date_utilities.py
+++ ambforecast/date_utilities.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def date_ordinal(d):
     """
     Returns the ordinal date suffix for a given integer.
@@ -20,14 +21,15 @@ def date_ordinal(d):
         >>> date_ordinal(4)
         'th'
     """
-    return ('th' if 4 <= d % 100 <= 20 else {1:'st', 2:'nd', 3:'rd'}.get(d % 10, 'th'))
+    return "th" if 4 <= d % 100 <= 20 else {1: "st", 2: "nd", 3: "rd"}.get(d % 10, "th")
+
 
 def next_monday():
     """
     Calculate the date of the next Monday after today.
 
     Returns:
-        pd.Timestamp: A pandas Timestamp object representing the date 
+        pd.Timestamp: A pandas Timestamp object representing the date
         of the next Monday after today's date.
 
     Example:
@@ -35,15 +37,16 @@ def next_monday():
         Timestamp('YYYY-MM-DD 00:00:00')
     """
     today = pd.Timestamp.today()
-    mondays = pd.date_range(today, freq='W-MON', periods=2).normalize().to_series()
+    mondays = pd.date_range(today, freq="W-MON", periods=2).normalize().to_series()
     return mondays.loc[mondays > today].iloc[0]
+
 
 def next_sunday():
     """
     Calculate the date of the next Sunday after today.
 
     Returns:
-        pd.Timestamp: A pandas Timestamp object representing the date 
+        pd.Timestamp: A pandas Timestamp object representing the date
         of the next Sunday after today's date.
 
     Example:
@@ -51,6 +54,5 @@ def next_sunday():
         Timestamp('YYYY-MM-DD 00:00:00')
     """
     today = pd.Timestamp.today()
-    sundays = pd.date_range(today, freq='W-SUN', periods=2).normalize().to_series()
+    sundays = pd.date_range(today, freq="W-SUN", periods=2).normalize().to_series()
     return sundays.loc[sundays > today].iloc[0]
- 
--- ambforecast/swast_forecast/utility.py
+++ ambforecast/swast_forecast/utility.py
@@ -1,9 +1,9 @@
-'''
+"""
 swast_forecast.utility
 
 utilities to help with forecasting
 
-'''
+"""
 
 import pandas as pd
 import numpy as np
@@ -14,9 +14,9 @@ from swast_forecast.ensemble import ProphetARIMAEnsemble
 
 
 def pre_process_daily_data(df, observation_col, index_col):
-    '''
-    Assumes daily data is stored in long format.  Read in 
-    and pivot to wide format so that there is a single 
+    """
+    Assumes daily data is stored in long format.  Read in
+    and pivot to wide format so that there is a single
     colmumn for each regions time series.
 
     Parameters:
@@ -30,119 +30,121 @@ def pre_process_daily_data(df, observation_col, index_col):
     index_col: str
          - the index columns.  Assume this is a date.
 
-    Returns:   
+    Returns:
     --------
         pd.DataFrame
-    '''
+    """
 
     df.set_index(index_col)
 
     df.columns = map(str.lower, df.columns)
     df.index.rename(str(df.index.name).lower(), inplace=True)
-    
-    clean_table = pd.pivot_table(df, values=observation_col.lower(), 
-                                 index=[index_col.lower()],
-                                 columns=['ora'], aggfunc=np.sum)
-    
-    clean_table.index.freq = 'D'
-    
+
+    clean_table = pd.pivot_table(
+        df,
+        values=observation_col.lower(),
+        index=[index_col.lower()],
+        columns=["ora"],
+        aggfunc=np.sum,
+    )
+
+    clean_table.index.freq = "D"
+
     return clean_table
 
 
-
-
 def get_best_arima_parameters():
-    '''
+    """
     Returns a dict containing the best arima
     parmaeters for the SWAST forecasting.
-    
+
     A new ambulance trust should run auto_arima
-    on their data to get their best parameters and 
+    on their data to get their best parameters and
     also CV a few models.
-    
+
     Returns:
     -------
         dict
-    
-    '''
-    params = {'order':(1,1,3),
-              'seasonal_order':(1,0,1,7)  
-    }
+
+    """
+    params = {"order": (1, 1, 3), "seasonal_order": (1, 0, 1, 7)}
     return params
 
 
-def default_ensemble(County,df_holiday=None):
-    '''
+def default_ensemble(County, df_holiday=None):
+    """
     Convenience function to create a ProphetARIMAEnsemble
-    using default best known parameters.  
-    '''
+    using default best known parameters.
+    """
     params = get_best_arima_parameters()
-    return ProphetARIMAEnsemble(order=params['order'], 
-                                seasonal_order=params['seasonal_order'], County = County,
-                                df_holiday = df_holiday)
+    return ProphetARIMAEnsemble(
+        order=params["order"],
+        seasonal_order=params["seasonal_order"],
+        County=County,
+        df_holiday=df_holiday,
+    )
 
 
 def forecast(y_train, horizon, alpha=0.05, return_all_models=False):
-    '''
-    Convenience function. All in one forecast function.  
+    """
+    Convenience function. All in one forecast function.
     Create a default ensemble fit the training data and predict ahead.
-    
+
     Parameters:
     --------
     y_train: pd.Series or pd.DataFrame
         y observations for training. Index is a DateTimeIndex
-        
+
     horizon: int
         forecast horizon
-        
+
     alpha: float, optional (default=0.05)
         100(1-alpha) prediction interval
-        
+
     return_all_models: bool. optional (default=False)
-        Return individual Regression with ARIMA error and Prophet 
-        model predictions 
-        
+        Return individual Regression with ARIMA error and Prophet
+        model predictions
+
     Returns:
     -------
         pd.DataFrame
-    '''
+    """
     model = default_ensemble()
     model.fit(y_train)
-    return model.predict(horizon, alpha=alpha, 
-                         return_all_models=return_all_models)
+    return model.predict(horizon, alpha=alpha, return_all_models=return_all_models)
 
 
-def multi_region_forecast(y_train, horizon, alpha=0.05, 
-                          return_all_models=False):
-    '''
+def multi_region_forecast(y_train, horizon, alpha=0.05, return_all_models=False):
+    """
     Run forecasts for all regions included in the training data.
-    
+
     This function exploits multiple CPUs. E.g. If your machine has 4 Cores then
-    It will run 4 regional forecasts in parrallel.  
-    
+    It will run 4 regional forecasts in parrallel.
+
     Parameters:
     --------
     y_train: pd.DataFrame
         Training data - each colunm in a region.
-        
+
     horizon: int
         forecast horizon
-        
+
     alpha: float, optional (default=0.05)
         100(1-alpha) prediction interval
-        
+
     return_all_models: bool. optional (default=False)
-        Return individual Regression with ARIMA error and Prophet 
-        model predictions 
-        
+        Return individual Regression with ARIMA error and Prophet
+        model predictions
+
     Returns:
     -------
-        List 
+        List
         Each item is a pd.DataFrame containing the forecast for a region.
         Items are ordered by columns in y_train
 
-    '''
+    """
     regions = y_train.columns.to_list()
-    results = Parallel(n_jobs=-1)(delayed(forecast)(y_train[region], horizon) 
-                                                    for region in regions)
+    results = Parallel(n_jobs=-1)(
+        delayed(forecast)(y_train[region], horizon) for region in regions
+    )
     return results

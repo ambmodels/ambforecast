3eb549f8391a6f55be833c60dda2ec5df8fd72c4
amyheather <a.heather2@exeter.ac.uk>
2026-08-04 17:03:06 +0100

    refactor(date_utilities): combine next_monday and next_sunday into a single function: next_weekday

--- src/ambforecast/date_utilities.py
+++ src/ambforecast/date_utilities.py
@@ -1,31 +1,25 @@
 import pandas as pd
 
 
-def next_monday():
-    """Calculate the date of the next Monday after today.
+def next_weekday(weekday):
+    """Calculate the date of the next given weekday after today.
+
+    Parameters
+    ----------
+    weekday : str
+        Three-letter pandas offset alias for the day of week,
+        e.g. "MON" for Monday, "SUN" for Sunday.
 
     Returns
     -------
     pd.Timestamp
-        Date of the next Monday after today's date.
+        Date of the next occurrence of the given weekday after today.
+
     """
     today = pd.Timestamp.today()
-    mondays = (
-        pd.date_range(today, freq="W-MON", periods=2).normalize().to_series()
+    days = (
+        pd.date_range(today, freq=f"W-{weekday}", periods=2)
+        .normalize()
+        .to_series()
     )
-    return mondays.loc[mondays > today].iloc[0]
-
-
-def next_sunday():
-    """Calculate the date of the next Sunday after today.
-
-    Returns
-    -------
-    pd.Timestamp
-        Date of the next Sunday after today's date.
-    """
-    today = pd.Timestamp.today()
-    sundays = (
-        pd.date_range(today, freq="W-SUN", periods=2).normalize().to_series()
-    )
-    return sundays.loc[sundays > today].iloc[0]
+    return days.loc[days > today].iloc[0]

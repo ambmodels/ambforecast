5630e5c7008dee943868dc664864cb5effbf4018
amyheather <a.heather2@exeter.ac.uk>
2026-08-07 11:05:02 +0100

    fix(ensemble): prevent running if all names not present (else silently run without calculating or missing things!)

--- src/ambforecast/ensemble.py
+++ src/ambforecast/ensemble.py
@@ -1,5 +1,6 @@
 """Ensemble forecast."""
 
+
 def calculate_ensemble(forecast_results, names):
     """Find the average forecast and intervals.
 
@@ -13,7 +14,17 @@ def calculate_ensemble(forecast_results, names):
     names : list[str]
         List of forecasts to include in ensemble. These are the names in the
         "name" column of the Forecaster.results dataframe.
+
     """
+    available_names = set(forecast_results["name"])
+    missing_names = [name for name in names if name not in available_names]
+
+    if missing_names:
+        raise ValueError(
+            "For ensemble, all requested forecast names must be present in "
+            f'forecast_results["name"]. Missing: {missing_names}'
+        )
+
     df = forecast_results[forecast_results["name"].isin(names)]
     ensemble = (
         df.groupby(["county", "currency", "ds"])

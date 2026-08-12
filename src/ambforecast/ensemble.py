8d471dbb1bb48e5f96f2468e233ee1ef66b7f07b
amyheather <a.heather2@exeter.ac.uk>
2026-08-12 10:33:51 +0100

    feat(ensemble): add metric and area to output dataframe

--- src/ambforecast/ensemble.py
+++ src/ambforecast/ensemble.py
@@ -13,25 +13,34 @@ def ensemble(forecasts):
 
     Returns
     -------
-    pd.DataFrame
+    ensemble : pd.DataFrame
         Ensemble forecast.
     """
-    # Each dataframe in the list should be the result for a single model,
-    # metric and area
+    metric = forecasts[0]["metric"].iloc[0]
+    area = forecasts[0]["area"].iloc[0]
+    dates = forecasts[0]["ds"]
+
     for forecast in forecasts:
         if forecast["ds"].duplicated().any():
             raise ValueError("Each forecast must have one row per date.")
 
-    # The provided forecasts must all cover the same dates
-    dates = forecasts[0]["ds"]
-    for forecast in forecasts[1:]:
+        if not (forecast["metric"] == metric).all():
+            raise ValueError("All forecasts must have the same metric.")
+
+        if not (forecast["area"] == area).all():
+            raise ValueError("All forecasts must have the same area.")
+
         if not forecast["ds"].equals(dates):
             raise ValueError("All forecasts must cover the same dates.")
 
     # Combine into single dataframe
-    return (
+    ensemble = (
         pd.concat(forecasts)
         .groupby("ds")
         .agg({"forecast": "mean", "pi_lower": "mean", "pi_upper": "mean"})
         .reset_index()
     )
+    ensemble.insert(1, "metric", metric)
+    ensemble.insert(2, "area", area)
+
+    return ensemble
